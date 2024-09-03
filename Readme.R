acfdabf4964b0cd86783883551a05aa14251f965
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 14:27:39 +0200

    Equation corrected and RMSE calculated

--- Readme.R
+++ Readme.R
@@ -7,4 +7,7 @@ Measured <- rawxlsx[1:25,1:2]
 Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]
 
 Para <- 1.911
-ParametEq <- 1.911*(1-2.718^(-Measured[,1]/Para))+0,1*Measured[,1]
+ParametEq <- 1.911*(1-2.718^(-Measured[,1]/Para))+0.1*Measured[,1]
+
+# Error
+sqrt(sum((Measured[,2]-ParametEq)^2)/nrow(Measured))

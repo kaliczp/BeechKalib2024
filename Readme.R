c8c327e84eb0b8c714b0a35317788b05ac344964
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 15:13:52 +0200

    Plot with funciton

--- Readme.R
+++ Readme.R
@@ -7,13 +7,15 @@ Measured <- rawxlsx[1:25,1:2]
 Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]
 
 Para <- 1.911
-ParametEq <- 1.911*(1-2.718^(-Measured[,1]/Para))+0.1*Measured[,1]
+ParametEq <- Para*(1-2.718^(-Measured[,1]/Para))+0.1*Measured[,1]
+ParametEqFun <- function(x) {1.911*(1-2.718^(-x/1.911))+0.1*x}
+ParametEqFun(Measured[,1])
 
-# Error
+## Error
 sqrt(sum((Measured[,2]-ParametEq)^2)/nrow(Measured))
 
 ## Figure
 par(las = 1)
 plot(Measured, pch = 16, col = "red",
      xlim = c( 0, 30), ylim = c(0,9))
-points(cbind(Measured[,1], ParametEq), col = 2)
+points(Measured[,1], ParametEqFun(Measured[,1]), col = "red")

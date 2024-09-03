d8bd789cb4067858e52cff7fd2bd65bcc08779af
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 14:44:39 +0200

    First version of figure

--- Readme.R
+++ Readme.R
@@ -11,3 +11,9 @@ ParametEq <- 1.911*(1-2.718^(-Measured[,1]/Para))+0.1*Measured[,1]
 
 # Error
 sqrt(sum((Measured[,2]-ParametEq)^2)/nrow(Measured))
+
+## Figure
+par(las = 1)
+plot(Measured, pch = 16, col = "red",
+     xlim = c( 0, 30), ylim = c(0,9))
+points(cbind(Measured[,1], ParametEq), col = 2)

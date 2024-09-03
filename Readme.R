fbba04977daee620bb2f63636a9853913b85a317
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 13:18:44 +0200

    Kucsara eq

--- Readme.R
+++ Readme.R
@@ -1,3 +1,7 @@
 library(readxl)
 
 rawxlsx <- as.data.frame(read_excel("Kalibráció bükk.xlsx", skip = 1))
+
+Measured <- rawxlsx[,1:2]
+
+Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]

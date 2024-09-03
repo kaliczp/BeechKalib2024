b17a0d3d933d40abb0ff3802b684f48e6a97d12c
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 13:19:26 +0200

    Parametrised equation

--- Readme.R
+++ Readme.R
@@ -5,3 +5,6 @@ rawxlsx <- as.data.frame(read_excel("Kalibráció bükk.xlsx", skip = 1))
 Measured <- rawxlsx[,1:2]
 
 Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]
+
+Para <- 1.911
+ParametEq <- 1.911*(1-2.718^(-Measured[,1]/Para))+0,1*Measured[,1]

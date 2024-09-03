d46d9da8681aa99c4cc996e0cf4d42ebfddf024f
Kalicz Péter <kaliczp@gmail.com>
2024-09-03 14:24:14 +0200

    Some NA (empty rows) are removed

--- Readme.R
+++ Readme.R
@@ -2,7 +2,7 @@ library(readxl)
 
 rawxlsx <- as.data.frame(read_excel("Kalibráció bükk.xlsx", skip = 1))
 
-Measured <- rawxlsx[,1:2]
+Measured <- rawxlsx[1:25,1:2]
 
 Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]
 

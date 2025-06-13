f42776e47143b65171d5b243180f14a1dd6e82c3
Kalicz Péter <kaliczp@gmail.com>
2025-06-13 10:51:17 +0200

    Axex labs corrected

--- Readme.R
+++ Readme.R
@@ -3,6 +3,7 @@ library(readxl)
 rawxlsx <- as.data.frame(read_excel("Kalibráció bükk.xlsx", skip = 1))
 
 Measured <- rawxlsx[1:25,1:2]
+names(Measured) <- c("Prec", "Interc")
 
 Kucsara <- 2.1*(1-2.718^(-Measured[,1]/2.1))+0.1*Measured[,1]
 
@@ -19,6 +20,8 @@ pdf(width = 16/2.54, height = 9/2.54, pointsize = 9)
 par(las = 1, mar = c(4.1,4.1,0.1,0.5),
     xaxs = "i", yaxs = "i")
 plot(Measured, pch = 16, col = "#4f81bd",
+     xlab = "Precipitation sum [mm]",
+     ylab = "Interception [mm]",
      xlim = c( 0, 30), ylim = c(0,9))
 curve(1.911*(1-2.718^(-x/1.911))+0.1*x, 0, 29, add = TRUE, lwd = 2)
 legend("topleft",c("Measured data", "Modified Merriam model"), pch = c(16,NA), lwd = c(NA, 2),col = c("#4f81bd", "black"))

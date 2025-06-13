3869d17565fd6405f0a326ad9788d44cd8d1ae04
Kalicz Péter <kaliczp@gmail.com>
2025-06-13 16:02:51 +0200

    MAE is added as optimisation funciton

--- Optimize.R
+++ Optimize.R
@@ -2,7 +2,13 @@ optInt <- function(Para, Prec, Int, Opt = "RMSE") {
     IntModel <- Para*(1-2.718^(-Prec/Para))+0.1*Prec
     if(Opt == "RMSE") {
         sqrt(mean((Int - IntModel)^2))
+    } else {
+        mean(abs(Int - IntModel)) #MAE
     }
 }
 
+## RMSE
 optimize(optInt, interval = c(1.5,2.5), Prec = Measured$Prec, Int = Measured$Int)
+
+## MAE
+optimize(optInt, interval = c(1.5,2.5), Prec = Measured$Prec, Int = Measured$Int, Opt = "MAE")
